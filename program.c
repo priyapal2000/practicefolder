2f33bf44b6571ae12f5f057038ac1a60903c394f
Ajay Sambhaji Deshmukh <78377310+ajstyle2410@users.noreply.github.com>
2024-07-14 01:38:13 +0530

    hello

---
+++ program.c
@@ -0,0 +1,16 @@
+/* #include <stdio.h>
+int main(){
+    int number;
+
+    printf(" ");
+    scanf("%d", & number);
+
+    printf("You entered: %d", number);
+    return 0;
+} */
+
+#include <stdio.h>
+int main(){
+    printf("Hello World");
+    return 0;
+}
